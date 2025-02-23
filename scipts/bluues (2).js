8bf37a7c6a44508b4044a501e7579e02d33e51d2
DaydainWasTaken <randomzytofficial@gmail.com>
2025-02-23 00:00:32 -0500

    e

---
+++ scipts/bluues (2).js
@@ -0,0 +1,11 @@
+-- Lua script (for environments like Roblox)
+print("Skibidi Toilet")
+
+-- Python script (for general-purpose Python interpreters)
+print("Skibidi Toilet")
+
+-- JavaScript script (for web browsers or Node.js environments)
+console.log("Skibidi Toilet");
+
+-- CS:GO Source Engine VScript (for use in CS:GO modding)
+Print("Skibidi Toilet\n");
